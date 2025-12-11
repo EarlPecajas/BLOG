30776f3456f983f7958710705fe514b0c0f3e42d
nikkou01 <jannicholas01@gmail.com>
2025-12-12 01:49:13 +0800

    Fix dev.nix to install dependencies before running server

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -11,18 +11,9 @@
     enable = true;
     previews = {
       web = {
-        command = ["python" "manage.py" "runserver" "0.0.0.0:$PORT"];
+        command = ["sh" "-c" "pip install -r requirements.txt && python manage.py migrate --noinput && python manage.py runserver 0.0.0.0:$PORT"];
         manager = "web";
       };
-    };
-  };
-  idx.workspace = {
-    onCreate = {
-      install-deps = "pip install -r requirements.txt";
-      migrate = "python manage.py migrate";
-    };
-    onStart = {
-      run-server = "python manage.py runserver 0.0.0.0:$PORT";
     };
   };
 }
